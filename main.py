e3c7448f1c56fcd88d50e1db72991c8d5017c6ee
Kirill Dunaev <83133484+saberkovich@users.noreply.github.com>
2023-03-04 20:43:35 +0500

    Вырезан вывод аптайма

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import asyncio
 from discord.ext import commands
 import pytz
 import datetime
-# Это простой комментарий чтобы комитать, 1
+# Это простой комментарий чтобы комитать
 
 def pon():
   for p in ['п','П','π','p']:
@@ -21,14 +21,14 @@ async def on_ready():
 
     time_str = time_now.strftime("%d.%m.%Y %H:%M:%S")
 
-    await bot.change_presence(activity=discord.Game(name="Включаюсь..."))
+    # await bot.change_presence(activity=discord.Game(name="Включаюсь..."))
     await bot.get_channel(1077307732757057656).send(f"Запуск завершен успешно! Время в которое включился бот [МСК]: `{time_str}`")
     print('BFG-bot готов к работе!')
 
     start_time = datetime.datetime.now()
 
     while True:
-      await bot.change_presence(activity=discord.Game(name=f"Аптайм: {str(datetime.datetime.now() - start_time).split('.')[0]}"))
+      # await bot.change_presence(activity=discord.Game(name=f"Аптайм: {str(datetime.datetime.now() - start_time).split('.')[0]}"))
 
       if datetime.datetime.now(timezone).strftime('%A_%H_%M_%S') == 'Saturday_20_00_00':
         await bot.get_channel(854994534391218176).send('<@783836872924987422>, <@940246956649873428>, <@711122945619263539>, <@874847454959378494>, <@1035626825277263902>, <@926917101355171852>, <@1027971828548903032>\Скидываем карту.')
