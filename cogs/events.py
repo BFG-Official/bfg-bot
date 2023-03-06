9e6c3c334556bee9a214db8bf7e1791cab122547
3l0iKoT <125878537+3l0iKoT@users.noreply.github.com>
2023-03-06 20:45:59 +0500

    Правка в тексте

--- cogs/events.py
+++ cogs/events.py
@@ -17,17 +17,17 @@ class Events(commands.Cog):
         time_str = time_now.strftime("%d.%m.%Y %H:%M:%S")
 
         # await commands.Bot.change_presence(self.client ,activity=discord.Game(name="Включаюсь..."))
-        await commands.Bot.get_channel(self.client, 1077307732757057656).send(f"Запуск завершен успешно! Время в которое включился бот [МСК]: `{time_str}` (ТЕСТОВЫЙ ЗАПУСК)")
+        await commands.Bot.get_channel(self.client, 1077307732757057656).send(f"Запуск завершен успешно! Время в которое включился бот [МСК]: `{time_str}`")
         print('BFG-bot готов к работе!')
         # start_time = datetime.datetime.now()
 
         while True:
             # await commands.Bot.change_presence(self.client, activity=discord.Game(name=f"Аптайм: {str(datetime.datetime.now() - start_time).split('.')[0]}"))
 
-            if datetime.datetime.now(timezone).strftime('%A_%H_%M_%S') == 'Saturday_20_00_00':
+            '''if datetime.datetime.now(timezone).strftime('%A_%H_%M_%S') == 'Saturday_20_00_00':
                 await commands.Bot.get_channel(self.client, 854994534391218176).send('<@783836872924987422>, <@940246956649873428>, <@711122945619263539>, <@874847454959378494>, <@1035626825277263902>, <@926917101355171852>, <@1027971828548903032>\Скидываем карту.')
             if datetime.datetime.now(timezone).strftime('%A_%H_%M_%S') == 'Sunday_10_00_00':
-                await commands.Bot.get_channel(self.client, 854994534391218176).send('тест')
+                await commands.Bot.get_channel(self.client, 854994534391218176).send('тест')'''
             
             await asyncio.sleep(1)
     
