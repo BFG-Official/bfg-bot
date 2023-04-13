a7203c8ad3c1646cdccc206ed468dd26bf3266b6
Kirill Dunaev <83133484+saberkovich@users.noreply.github.com>
2023-04-14 01:23:30 +0500

    Система игровых варнингам смотрителям

--- cogs/game_chat_warnings.py
+++ cogs/game_chat_warnings.py
@@ -1,7 +1,6 @@
-import discord
 from discord.ext import commands
 
-words = ['тест1', 'тест2', 'тест3']
+words = ['даун', 'долбaёб', 'долбаеб', 'нaхуй', 'даунихa', 'ебaл', 'шлюхa', 'пидор', 'пидорaс', 'негрилa', 'хуесос', 'хуесосинa', 'сучкa', 'проституткa', 'аутист', 'гандон', 'педик', 'мразь', 'тварь', 'гитлер', 'украина', 'украину', 'украины', 'россия', 'россию', 'россии', 'трахал', 'урод']
 channel_id = 1048513739558752276
 
 class game_chat_warnings(commands.Cog):
@@ -9,10 +8,10 @@ class game_chat_warnings(commands.Cog):
     def __init__(self, client):
         self.client = client
     
-    # @commands.Cog.listener()
-    # async def on_message(self, message):
-    #     if message.channel.id == channel_id and any(word in message.content for word in words):
-    #         await message.reply('<@&1032200538516901899>\nСистема заметила сообщения которые возможно нарушают правила.')
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.channel.id == channel_id and any(word in message.content.lower() for word in words):
+            await message.reply('<@&1032200538516901899>\nСистема заметила сообщения которые возможно нарушают правила.')
 
 async def setup(client):
     await client.add_cog(game_chat_warnings(client))
