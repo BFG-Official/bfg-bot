adf5e53bd571aac78049d43efd07b184463d9ade
Kirill Dunaev <83133484+saberkovich@users.noreply.github.com>
2023-05-17 09:47:57 +0500

    Обновление технической информации

--- cogs/system_status.py
+++ cogs/system_status.py
@@ -16,10 +16,15 @@ class system_status(commands.Cog):
     async def update_stats(self):
         cpu_usage = psutil.cpu_percent()
         ram_usage = psutil.virtual_memory().percent
+        total_memory = psutil.virtual_memory().total
 
         channel = self.client.get_channel(1108251768992837734)
         message = await channel.fetch_message(self.message_id)
-        await message.edit(content=f"CPU Usage: {cpu_usage}%\nRAM Usage: {ram_usage}%")
+        embed = discord.Embed(title="System Status", color=discord.Color.blue())
+        embed.add_field(name="ЦП", value=f"{cpu_usage}%")
+        embed.add_field(name="ОЗУ", value=f"{ram_usage}%")
+        embed.add_field(name="Всего памяти", value=f"{total_memory} байт")
+        await message.edit(embed=embed)
 
 async def setup(client):
     await client.add_cog(system_status(client))
